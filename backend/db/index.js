35fe677275864f8624138fa0b3eb2f94a0a9b134
kimsengthai <kimseng.thai@hotmail.com>
2024-03-12 03:45:32 +1100

    today

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,14 +1,8 @@
 import pg from "pg"; //Import pg
 const { Pool } = pg; //const pool from pg
 
-const pool = new Pool({
-  //database authentification set up, for attack purposes, put it in .env
-  host: "localhost",
-  user: "postgres",
-  password: "lolimthebest",
-  port: 5432,
-  database: "Yelp",
-});
+const pool = new Pool();
+//database authentification set up, for attack purposes, put it in .env
 
 export const query = (text, params, callback) => {
   //Call back to main file
--- backend/server.js
+++ backend/server.js
@@ -10,39 +10,67 @@ app.use(express.json());
 app.use(cors());
 
 //GET all restaurants
-app.get("/getRestaurants", (req, res) => {
-  //sending back to the client
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurants: ["mcdonals", "KFC"],
-    },
-  });
+app.get("/getRestaurants", async (req, res) => {
+  //in express, we always wrap asyn with try catch block
+  try {
+    const results = await db.query("select * FROM restaurants");
+    console.log(results);
+    //sending back to the client
+    res.status(200).json({
+      status: "success",
+      //best practice to returns the size of the rows
+      results: results.rows.length,
+      data: {
+        //rows because that is where the data is store
+        restaurants: results.rows,
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 //GET individual restaurants
-app.get("/getRestaurants/:id", (req, res) => {
-  //params value from request which is :id, anything after : is params
-  console.log(req.params);
-  //sending back to the client
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurants: "mcdonals",
-    },
-  });
+app.get("/getRestaurants/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const results = await db.query("SELECT * FROM restaurants WHERE id = $1", [
+      id,
+    ]);
+    //sending back to the client
+    //sending back to the client
+    res.status(200).json({
+      status: "success",
+      //best practice to returns the size of the rows
+      results: results.rows.length,
+      data: {
+        //rows because that is where the data is store
+        restaurants: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 //POST create restaurants
-app.post("/getRestaurants", (req, res) => {
+app.post("/getRestaurants", async (req, res) => {
   console.log(req.body);
-  //sending back to the client
-  res.status(201).json({
-    status: "success",
-    data: {
-      restaurants: "mcdonals",
-    },
-  });
+  try {
+    const results = await db.query(
+      "INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) RETURNING *",
+      [req.body.name, req.body.location, req.body.price_range]
+    );
+    //sending back to the client
+    res.status(201).json({
+      status: "success",
+      data: {
+        restaurants: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 //PUT update restaurants
